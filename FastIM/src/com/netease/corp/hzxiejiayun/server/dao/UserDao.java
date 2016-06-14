c37f56bbed84db16fe25b95b27a1b0c165d23d07
hzxiejiayun <furnace09@gmail.com>
2016-06-14 20:45:32 +0800

    add some database support

---
+++ FastIM/src/com/netease/corp/hzxiejiayun/server/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.netease.corp.hzxiejiayun.server.dao;
+
+/**
+ * Created by hzxiejiayun on 2016/6/14.
+ */
+public class UserDao {
+}
---
+++ FastIM/src/com/netease/corp/hzxiejiayun/server/dao/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by hzxiejiayun on 2016/6/14.
+ *
+ * 主要用来处理数据库方面的操作
+ */
+package com.netease.corp.hzxiejiayun.server.dao;
