dbcc649323bb57121277504f32e92e6f88bfb652
hzxiejiayun <furnace09@gmail.com>
2016-06-20 10:55:09 +0800

    useful trim implementation in StringUtils

--- FastIM/src/main/java/com/netease/corp/hzxiejiayun/common/util/StringUtils.java
+++ FastIM/src/main/java/com/netease/corp/hzxiejiayun/common/util/StringUtils.java
@@ -6,8 +6,35 @@ package com.netease.corp.hzxiejiayun.common.util;
 public class StringUtils {
 
     public static String trim(String str) {
-        String result = null;
+        if (str == null) {
+            return null;
+        }
+        if (str.length() == 0) {
+            return "";
+        }
+        char tmp[] = str.toCharArray();
+        for (int i=0, j=tmp.length-1; i<j; ) {
+            if (tmp[i] == ' ') {
+                i++;
+                continue;
+            }
+            if (tmp[j] == ' ') {
+                j--;
+                continue;
+            }
+            return str.substring(i, j+1);
+        }
+        return "";
+    }
 
-        return result;
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(StringUtils.trim(s));
+        s = "";
+        System.out.println(StringUtils.trim(s));
+        s = " c d    ";
+        System.out.println(StringUtils.trim(s) + StringUtils.trim(s).length());
+        s = "  a b c   ";
+        System.out.println(StringUtils.trim(s) + StringUtils.trim(s).length());
     }
 }
